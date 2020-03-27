7ef445155732904c9495c1c17f2d4770cb027498
felipe <feliperarnau@gmail.com>
2020-03-27 18:39:55 -0300

    add everything before linking index

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const app = express();
 
 
 app.get("/", function(req, res) {
-
+  var appId = "88955fcb41de59dfa20bdf62950678b9";
+  var units = "metric";
   var city = "paris";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=88955fcb41de59dfa20bdf62950678b9&units=metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appId + "&units=" + units;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -18,7 +19,7 @@ app.get("/", function(req, res) {
       const weatherData = JSON.parse(data);
       var icon = weatherData.weather[0].icon;
       var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      var clima = "The temperature in "+ city +"  is: " + weatherData.main.temp + " degrees celcius and the weather is: " + weatherData.weather[0].description;
+      var clima = "The temperature in " + city + "  is: " + weatherData.main.temp + " degrees celcius and the weather is: " + weatherData.weather[0].description;
       res.write("<h1>" + clima + "</h1>");
       res.write("<img src='" + iconURL + "'/>");
       res.send();
