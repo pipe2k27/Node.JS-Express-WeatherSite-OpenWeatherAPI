91fb5b829c4c8a0b4eee2cbec1ae454f413eeb0a
felipe <feliperarnau@gmail.com>
2020-03-27 17:55:37 -0300

    first

---
+++ app.js
@@ -0,0 +1,39 @@
+// jshint esversion:6
+const express = require("express");
+
+const https = require("https");
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=buenos aires&appid=88955fcb41de59dfa20bdf62950678b9&units=metric";
+
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      var icon = weatherData.weather[0].icon;
+      var iconURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+      var clima = "The temperature in buenos aires is: " + weatherData.main.temp + " degrees celcius and the weather is: " + weatherData.weather[0].description;
+      res.write("<h1>" + clima + "</h1>");
+      res.write("<img src='" + iconURL+ "'/>");
+      res.send();
+
+    });
+  });
+
+
+});
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("server is running on port 3000");
+});
